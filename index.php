974dd0188c0248f72ca76809f54c91808d2c7827
dalailomo <reisereise.85@gmail.com>
2015-11-04 22:29:19 +0000

    code 4 dollars cheaper

--- index.php
+++ index.php
@@ -14,15 +14,10 @@ use \Sandbox\Representation\Board;
 
 $hangar = new Scenario('Hangar');
 
-$cacodemon = new Cacodemon;
-$cacodemon2 = new Cacodemon;
-$imp = new Imp;
-$zombieMan = new ZombieMan;
-
-$hangar->place($cacodemon, new Coordinate(1, 8));
-$hangar->place($cacodemon2, new Coordinate(5, 15));
-$hangar->place($imp, new Coordinate(2, 19));
-$hangar->place($zombieMan, new Coordinate(18, 18));
+$hangar->place(new Cacodemon, new Coordinate(1, 8));
+$hangar->place(new Cacodemon, new Coordinate(5, 15));
+$hangar->place(new Imp, new Coordinate(2, 19));
+$hangar->place(new ZombieMan, new Coordinate(18, 18));
 
 $board = new Board($hangar, new Coordinate(0, 0), new Coordinate(20, 20));
 echo $board->render();
